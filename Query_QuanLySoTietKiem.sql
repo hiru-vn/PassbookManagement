4c79823a8044cc2d7e95e8448217aed4b0b7e781
Huy Tran <45990022+tranhiepnguyenhuy1999@users.noreply.github.com>
2019-04-20 22:42:09 +0700

    Renew query_Quanlysotietkiem
    Sửa lại theo query mẫu m đưa, nếu có j sai sót nói t nhe

--- Query_QuanLySoTietKiem.sql
+++ Query_QuanLySoTietKiem.sql
@@ -1,50 +1,53 @@
-﻿CREATE DATABASE SAVINNG_PASSBOOK
-CREATE TABLE CUSTOMER(
- ID CHAR(6) PRIMARY KEY,
- NAME NVARCHAR(40),--Ten Khach hang
- CUS_ADDRESS NVARCHAR(40),-- Dia chi
- SAVINGBOOK CHAR(6),-- Ma STK
- CMND CHAR(9)
-);
-GO
-CREATE TABLE SAVINGS_ACCOUNT(
-ID CHAR(6) PRIMARY KEY,
-OPENDATE DATE,-- Ngay mo so
-ID_CUS CHAR(6),-- Ma khach hang
-ACCOUNT_BALANCE MONEY,-- So du
-TYPE_ACCOUNT CHAR(4)--Ma loai TK
-);
-GO
-CREATE TABLE TYPE_ACCOUNT (
-ID CHAR(6) PRIMARY KEY,
-TYPENAME VARCHAR(40),
-INTEREST_RATE FLOAT,-- Lai suat
-LIMIT_TIME INT-- So Thang thoi han lai suat
-);
-GO
-CREATE TABLE COLLECTBILL (
-ID CHAR(6) PRIMARY KEY,
-ID_PASSBOOK CHAR(6),
-COLLECT_MONEY MONEY,
-EXDATE DATE,--Ngay goi
-);
-GO
-CREATE TABLE WITHDRAWBILL (
-ID CHAR(6) PRIMARY KEY,
-ID_PASSBOOK CHAR(6),
-EXDATE DATE,
-DRAW_MONEY MONEY-- Ngay rut
-);
-GO
-ALTER TABLE SAVINGS_ACCOUNT ALTER COLUMN TYPE_ACCOUNT CHAR(6) 
-ALTER TABLE  SAVINGS_ACCOUNT ADD CONSTRAINT FK_SAVINGSACCOUNT_IDCUS FOREIGN KEY (ID_CUS) REFERENCES CUSTOMER (ID);
-ALTER TABLE  SAVINGS_ACCOUNT ADD CONSTRAINT FK_SAVINGSACCOUNT_TYPEACCUONT FOREIGN KEY (TYPE_ACCOUNT) REFERENCES TYPE_ACCOUNT (ID);
-ALTER TABLE  COLLECTBILL ADD CONSTRAINT FK_COLLECTBILL_IDPASSBOOK FOREIGN KEY (ID_PASSBOOK) REFERENCES SAVINGS_ACCOUNT (ID);
-ALTER TABLE  WITHDRAWBILL ADD CONSTRAINT FK_WITHDRAWBILL_IDPASSBOOK FOREIGN KEY (ID_PASSBOOK) REFERENCES SAVINGS_ACCOUNT (ID);
-GO
- CREATE TABLE PARAMETER 
- (
- AT_LEAST_MONEY_FST MONEY,--So tien goi ban dau toi thieu
- AT_LEAST_EXTRA_MONEY MONEY-- So tien goi toi thieu
- )
- GO
+drop database savingpassbook
+create database savingpassbook
+go
+use savingpassbook
+go
+create table customer
+(
+id nvarchar(20) primary key,
+cus_name nvarchar(200) not null,
+cus_address nvarchar(200),
+cmnd char(9) not null
+)
+
+create table passbook
+(
+id int identity(1,1) primary key,
+passbook_balance money not null,
+passbook_type int not null,
+passbook_customer nvarchar(20) not null,
+opendate datetime default getdate()
+)
+
+create table typepassbook
+(
+id nvarchar(20) primary key,
+typename nvarchar(200) not null,
+interest_rate float not null,
+term int default 0,
+)
+create table collectbill
+(
+id nvarchar(20) primary key,
+collect_passbook int not null,
+collectmoney  money not null,
+collectdate datetime default getdate(),
+)
+create table withdrawbill
+(
+id nvarchar(20) primary key,
+withdraw_passbook int not null,
+wihtdrawmoney money not null	,
+withdrawdate datetime default getdate()
+)
+create table parameter
+(
+atleast_collectmoney money default 0,
+atleast_passbookbalance money default 0	
+)
+alter table passbook add constraint fk_customer_id foreign key(passbook_customer) references customer(id)
+alter table passbook add constraint fk_typepassbook_id foreign key(passbook_type) references typeaccount(id)
+alter table collectbill add constraint fk_passbook_id foreign key (collect_passbook) references passbook(id)
+alter table withdrawbill add constraint fk_passbook_id foreign key (withdraw_passbookt) references passbook(id)
+
